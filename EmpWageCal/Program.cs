83653a8808490b29b4b3397bba306a87577f04d4
akshata <akshata2018p@gamil.com>
2023-02-03 21:13:16 +0530

    [akshata]Add:EmpWageCalculation

---
+++ EmpWageCal/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageCal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Emp wage Calculation");
+            Console.ReadLine();
+        }
+    }
+}
