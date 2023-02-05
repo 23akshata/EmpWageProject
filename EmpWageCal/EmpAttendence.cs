1a67e02fcb6d3030c046e31f992273205e8b15a3
akshata <akshata2018p@gamil.com>
2023-02-05 12:11:35 +0530

    [akshata]Add:EmpPartTimeWage

--- EmpWageCal/EmpAttendence.cs
+++ EmpWageCal/EmpAttendence.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,18 +9,24 @@ namespace EmpWageCal
 {
     public class CheckAttendances
     {
-        public void Attendances ()
+        public void Attendances()
         {
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HRS = 20;
             int empHrs = 0;
             int empWages = 0;
             Random random = new Random();
-            int empcheck = random.Next(0, 2);
+            int empcheck = random.Next(0, 3);
             if (empcheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee Is Present");
+                Console.WriteLine("Employee Is Present Full Time");
                 empHrs = 8;
+            }
+            else if (empcheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee Is Present Part Time ");
+                empHrs = 4;
             }
             else
             {
